bca14c38a52016d6e0eeee78eec520c8fd7bafd5
ksing419 <300878370@cencol.ca>
2016-06-03 18:54:09 -0400

    latest js

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,30 +3,20 @@ var homepage = [
     "I was born in India on March 22, 1996. At the age of 5 I moved to Italy were I lived for 14 years. I completed all my studies there and after getting my high school diploma in I.T., I decided to move to Canada to obtain a higher qualification in the technology field. I've been always interested in computers and software developing and I hope to get a good job in this field." 
 ];
 
-var projects = [
-    "My projects"
-]
 
-(function(){
+(function(){  
     
-    var paragraphElements = [];
-    
-    paragraphElements[0] = document.getElementById("title1");
-    paragraphElements[1] = document.getElementById("content1");
-    paragraphElements[2] = document.getElementById("title2");
-    
-    var paragraphElementsLength = paragraphElements.length;
-    
-    for (var index = 0; index <= paragraphElementsLength; index++) {
-        if(paragraphElements[index]) {
-         paragraphElements[index].innerHTML = homepage[index];
-        }
+    var ids = [];
+    ids[0] = document.getElementById('title1').textContent;
+    ids[0] = document.getElementById('content1').textContent;
+    alert("as");
+    for (var i = 0; i < ids.length; i++) {
+        alert(document.getElementById(ids[i]).textContent);
+         //= homepage[i];
+        alert("a");
     }
     
-    
-    /*if () {
-        
-    }
+    /*
     var title1 = document.getElementById('title1').textContent;
     var content1 = document.getElementById('content1').textContent;
     var title1 = document.getElementById('title2').textContent;
