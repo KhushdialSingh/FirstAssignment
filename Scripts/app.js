91304cd7889d459a79b12d192309e897fae3b6aa
ksing419 <300878370@cencol.ca>
2016-06-03 17:47:58 -0400

    latest changes

---
+++ Scripts/app.js
@@ -0,0 +1,13 @@
+var homepage = [
+    "Khushdial Singh",
+    "I was born in India on March 22, 1996. At the age of 5 I moved to Italy were I lived for 14 years. I completed all my studies there and after getting my high school diploma in I.T., I decided to move to Canada to obtain a higher qualification in the technology field. I've been always interested in computers and software developing and I hope to get a good job in this field." 
+];
+
+(function(){
+    
+    document.getElementById('title1').textContent = homepage[0];
+    document.getElementById('content1').textContent = homepage[1];
+    
+    
+    
+})();
